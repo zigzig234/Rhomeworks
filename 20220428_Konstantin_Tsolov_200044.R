61eac68d00b7647145131ee12b83a166353f7dc6
zigzig234 <101800369+zigzig234@users.noreply.github.com>
2022-06-19 17:38:01 +0300

    Update 20220428_Konstantin_Tsolov_200044.R

--- 20220428_Konstantin_Tsolov_200044.R
+++ 20220428_Konstantin_Tsolov_200044.R
@@ -55,4 +55,56 @@ prm_num = function(n) {
 } 
 prm_num(100)
 
-#No time for problem 3, will try to do it later
+#####Problem 3#####
+# Read the wikipedia article and investopedia article on MACD:
+# https://en.wikipedia.org/wiki/MACD
+# https://www.investopedia.com/terms/m/macd.asp
+
+# Download data for a stock of your choice and do the following:
+# 1.Calculate the 26-period EMA(use the EMA function from tidyquant)
+# 2.Calculate the 12-period EMA.
+# 3.Calculate the MACD line(12-period EMA minus 26-period EMA)
+# 4.Calculate the signal line - this is the 9-period EMA of the MACD.
+# 5.Calculate the buy/sell signals. This means create a new column which tell
+# us if we should buy or sell. When the MACD line crosses the signal line
+# from above(MACD is above signal then MACD is below signal) this is a sell signal. 
+# If it crosses from below (MACD is below signal then MACD is above signal) this is a buy signal.
+# 6. Simulate how the strategy preforms and compare it to a benchmark strategy
+# of just buying and holding the stock.
+# In order to do this start with a portfolio of 100$ invested in the stock on the first day
+# and see how it performs. Example:
+# I start with 100$ and a stock which costs 100$ at the beginning of my time period.
+# I get a buy signal when the stock price is 90. I buy the stock.
+# I get a sell signal to sell the stock when the price is 110. I sell it and 
+# and don't get any more signals.I end up with 100 * 110 / 90 = 122.22 
+# The benchmark portfolio is I buy the stock at 100 at the beginning and at
+# the end of the period the stock price is 120. I end up with 120.
+# 122.22 > 120. so the MACD strategy was beating the market.
+#####Problem 3#####
+
+library(quantmod)
+library(tidyquant)
+library(tidyverse)
+
+inital_data <- tq_get("AAPL") %>%
+  mutate(EMA26 = EMA(adjusted, n =26),
+         EMA12 = EMA(adjusted, n =12),
+         MACDline = EMA12 - EMA26,
+         SignalLine = EMA(MACDline, n=9)) %>%
+  filter(!is.na(EMA26 & SignalLine)) %>%
+  mutate(signal = case_when(lag(MACDline) > lag(SignalLine) & MACDline < SignalLine ~ "sell",
+                            lag(MACDline) < lag(SignalLine) & MACDline > SignalLine ~ "buy",
+                            TRUE ~ "hold"))
+
+inital_data <- inital_data %>%
+  mutate(BenchmarkMoney = 100,
+         sss = adjusted / lag(adjusted),
+         sss = ifelse(is.na(sss), 1, sss),
+         BenchmarkMoney1 = cumprod(sss),
+         StrategyMoney = 100,
+         sss1 = case_when(signal = "do not have stocks" ~ 1,
+                          signal = "have stock" ~ sss,
+                          TRUE ~ 999999999),
+         StrategyMoney1 = cumprod(sss1))
+
+options(scipen = 999)
