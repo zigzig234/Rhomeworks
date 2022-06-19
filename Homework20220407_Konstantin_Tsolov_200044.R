ee3568194febfd4ef447776b32d03729dff31c90
zigzig234 <101800369+zigzig234@users.noreply.github.com>
2022-06-19 17:32:14 +0300

    Update Homework20220407_Konstantin_Tsolov_200044.R

--- Homework20220407_Konstantin_Tsolov_200044.R
+++ Homework20220407_Konstantin_Tsolov_200044.R
@@ -20,9 +20,37 @@ library(tidyverse)
 library(tidyquant)
 library(padr)
 library(dplyr)
-stocks = tq_get(c("NFLX","FB","AMZN"), get = "stock.prices", from = "2019-01-01", to  = "2021-04-01") %>%
-  select(c(symbol,date,adjusted)) %>%
-  pad() %>%
-  fill(c(symbol,adjusted),.direction = 'up');
-select_stocks=tq_get(c("FB","AMZN"),get = "stock.prices", from = "2019-01-01", to = "2019-07-01") %>% arrange(symbol,desc(date))
+library(lubridate)
+
+stocks = tidyquant::tq_get(c("NFLX","FB","AMZN"), get = "stock.prices", from = "2019-01-01", to  = "2021-04-01") %>%
+  select(c(symbol,date,adjusted));
+
+dates <- data.frame(Date = rep(seq.Date(from = ymd('2019-01-01'), 
+                                    to = ymd('2021-04-01'), 
+                                    by = 'days'), 3),
+                    Symbol = c(rep("AMZN", 822),rep("FB", 822),rep("NFLX", 822)))
+
+joint_together <- dates %>%
+  dplyr::left_join(stocks, by = c("Date" = "date", "Symbol" = "symbol")) %>%
+  group_by(Symbol) %>%
+  fill(adjusted, .direction = "downup")
+
+DF <- joint_together %>%
+  filter(Symbol %in% c("AMZN", "FB"),
+         between(Date, ymd("2019-01-01"), ymd("2019-07-01")) | 
+         between(Date, ymd("2020-04-01"), ymd("2020-07-01"))) %>%
+  dplyr::arrange(Symbol, desc(Date))
   
+x <- DF %>%
+ filter((Date == min(ymd(Date)) | Date == max(ymd(Date))) &
+          (Symbol == "AMZN" | Symbol == "FB"))
+
+a <- NULL
+a <- DataFrame %>%
+  mutate(year = substring(Date, 1, 4),
+         month = substring(Date, 6, 7),
+         day = substring(Date, 9, 10)) %>%
+  group_by(year, month) %>%
+  filter(day == max(day)) %>%
+  arrange(year, month)
+view(a)
