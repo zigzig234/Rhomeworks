29dfc844864333f3bd13094539afe7c70e9db8f7
zigzig234 <101800369+zigzig234@users.noreply.github.com>
2022-06-19 17:45:36 +0300

    Update Homework20220519_Konstantin_Tsolov_200044.R

--- Homework20220519_Konstantin_Tsolov_200044.R
+++ Homework20220519_Konstantin_Tsolov_200044.R
@@ -22,21 +22,21 @@ library(data.frame)
 FromDate<-"2012-01-03"
 ToDate<-"2021-10-26"
 
-info<-tidyquant::tq_get("GOOG", from = lubridate::ymd(FromDate),
+data<-tidyquant::tq_get("AMZN", from = lubridate::ymd(FromDate),
                         to=lubridate::ymd(ToDate))%>%
   dplyr::select(symbol, date, adjusted)
 
 dates<-base::data.frame(Dates=base::rep(base::seq.Date(from = lubridate::ymd("2012-01-03"),
                                                        to=lubridate::ymd("2021-10-26"),
                                                        by="day")),
-                        Symbol = c(base::rep("GOOG",3585)))
+                        Symbol = c(base::rep("AMZN",3585)))
                                 
-DATA<-dates%>%
-  dplyr::left_join(info, by=c("Dates"="date", "Symbol" = "symbol"))%>%
+data2<-dates%>%
+  dplyr::left_join(data, by=c("Dates"="date", "Symbol" = "symbol"))%>%
   dplyr::group_by(Symbol)%>%
   tidyr::fill(adjusted, .direction = "downup")
 
-SMA<-DATA%>%
+SMA<-data2%>%
   dplyr::mutate(sma20 = TTR::SMA(adjusted, n= 20),
                 Stand.Dev. = RcppRoll::roll_sdr(adjusted, n= 20),
                 Upper.Bound = sma20 + 2 * Stand.Dev.,
@@ -51,19 +51,19 @@ SMA<-DATA%>%
          
 #####Problem 2#####
 
-info2<-tidyquant::tq_get("GOOG", from = lubridate::ymd(FromDate),
+data3<-tidyquant::tq_get("AMZN", from = lubridate::ymd(FromDate),
                                                 to=lubridate::ymd(ToDate))%>%
                           dplyr::select(symbol, date, adjusted)            
  
-RSIdata<-info2%>%
+rsidata<-data3%>%
   mutate(Gains=ifelse(adjusted > lag(adjusted),adjusted - lag(adjusted), "0"),
          Losses = ifelse(adjusted < lag(adjusted),adjusted - lag(adjusted), "0"))%>%
   slice(2:2470)
 
-RSIdata$Gains<-as.numeric(as.character(RSIdata$Gains))
-RSIdata$Losses<-as.numeric(as.character(RSIdata$Losses))
+rsidata$Gains<-as.numeric(as.character(rsidata$Gains))
+rsidata$Losses<-as.numeric(as.character(rsidata$Losses))
 
-RSIdata2<-RSIdata%>%
+rsidata2<-rsidata%>%
   mutate(Gains.RSI = RcppRoll::roll_sumr(Gains, n= 14)/14,
          Losses.RSI = RcppRoll::roll_sumr(Losses, n= 14)/14,
          RSI = 100 - (100/(1 + (Gains.RSI/Losses.RSI))),
